67020a518a3ec0dd42fb7f097c7f07157ca15c6f
tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2020-08-07 13:13:44 +0700

    update
    update

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 <div id="content_wrapper1">
-    <div id="bg_footer" class="pull-left">
+    <div id="bg_footer" class="pull-left" style="background-image: url('img/footer.png');">
+    <p><strong class="text-white" style="font-size: 25px; color:white; margin-top:300px;margin-left:300px;">ศูนย์ถ่ายทอดเทคโนโลยีและวิศวกรรม</strong></p>
+        <p><strong class="text-white" style="font-size: 25px; color:white; margin-top:300px;margin-left:300px;">Technology and Engineering Transfer Center </strong></p>
     </div>
 </div>
 </body>
